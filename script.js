69c31e8c4fb0b449b8ceddc315a3655a4d96edf6
Iamneo <techadmin@iamneo.ai>
2023-09-03 12:43:54 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ let gameboard=['','','','','','','','',''];
 let gameactive=true;
 const cells=document.querySelectorAll('.cell');
 const resetbutton=document.getElementById('reset-button');
-const resultmessage=document.getElementById('result-')
+const resultmessage=document.getElementById('result-message');
+function handlecellclick(event){
+    const cell=event.target;
+    const cellindex=parseInt(cell.id('-')[1]);
+    if(gameboard[cellindex]==''&&gameactive){
+        cell.textcontent=currentplayer;
+        gameboard[cellindex]=currentplayer;
+        if(checkwin()){
+            gameactive=false;
+            resultmessage.textcontent='Player ${currentplayer} wins!';
+            enableresetbutton();
+        }
+        else if(checkdraw()){
+            gameactive=false;
+            resultmessage.textcontent="It's a draw!";
+            enableresetbutton();
+        }
+        else{
+            currentplayer=currentplayer=='X'
+        }
+    }
+}
